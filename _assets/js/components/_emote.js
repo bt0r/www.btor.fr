70e664c4d0aeceb4cc58c1516e6bec95f7894779
btor <the.btor@gmail.com>
2020-01-18 16:20:11 +0100

    fix: ESlint rules

--- _assets/js/components/_emote.js
+++ _assets/js/components/_emote.js
@@ -1,16 +1,15 @@
-import $ from 'jquery';
 const Emote = {
-    init () {
-        let body = document.body.innerHTML;
-        //document.body.innerHTML = document.body.innerHTML.replace('@@LUL', 'hi');
-        const emotes = [...body.matchAll('@@([A-Z]+)')];
-        emotes.forEach(function(emote) {
-            const emoteKey = emote[1];
-            const path = `/assets/images/emote/${emoteKey}.png`;
-            const img = `<img src="${path}" class="emote"/>`;
-            body = body.replace(emote[0],img);
-        });
-        document.body.innerHTML = body;
-    }
+  init () {
+    let body = document.body.innerHTML;
+    //document.body.innerHTML = document.body.innerHTML.replace('@@LUL', 'hi');
+    const emotes = [...body.matchAll(/@@([A-Z]+)/)];
+    emotes.forEach(function(emote) {
+      const emoteKey = emote[1];
+      const path = `/assets/images/emote/${emoteKey}.png`;
+      const img = `<img src="${path}" class="emote" alt=""/>`;
+      body = body.replace(emote[0],img);
+    });
+    document.body.innerHTML = body;
+  }
 };
 export default Emote;
